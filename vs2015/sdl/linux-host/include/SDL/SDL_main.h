d7ef1b0164244696dcab91f747a22710a5d69235
Jonathan Campbell <jonathan@castus.tv>
2018-12-27 10:34:24 -0800

    cleanup

--- vs2015/sdl/linux-host/include/SDL/SDL_main.h
+++ vs2015/sdl/linux-host/include/SDL/SDL_main.h
@@ -56,7 +56,7 @@
 #define main	SDL_main
 
 /** The prototype for the application's main() function */
-extern C_LINKAGE int SDL_main(int argc, char *argv[]) SDL_MAIN_NOEXCEPT;
+extern C_LINKAGE int SDL_main(int argc, char *argv[]);
 
 
 /** @name From the SDL library code -- needed for registering the app on Win32 */
