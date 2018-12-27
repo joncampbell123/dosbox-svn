1f70007b36d54a9e28e8b268ff324ebaf6c5fab9
Jonathan Campbell <jonathan@castus.tv>
2018-12-27 10:36:49 -0800

    Clang does not like the noexcept spec here

--- vs2015/sdl/linux-host/include/SDL/SDL_main.h
+++ vs2015/sdl/linux-host/include/SDL/SDL_main.h
@@ -56,7 +56,7 @@
 #define main	SDL_main
 
 /** The prototype for the application's main() function */
-extern C_LINKAGE int SDL_main(int argc, char *argv[]);
+extern C_LINKAGE int SDL_main(int argc, char *argv[]) SDL_MAIN_NOEXCEPT;
 
 
 /** @name From the SDL library code -- needed for registering the app on Win32 */
