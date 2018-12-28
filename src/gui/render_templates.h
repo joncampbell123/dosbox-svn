9ab54f874fb204d0fbaf2677849f5508945c5391
Jonathan Campbell <jonathan@castus.tv>
2018-12-27 16:33:28 -0800

    Backport DOSBox-X Mac OS X BGRA fixes in the render scaler code

--- src/gui/render_templates.h
+++ src/gui/render_templates.h
@@ -99,7 +99,11 @@
 #elif DBPP == 16
 #define PMAKE(_VAL) (((_VAL) & 31) | ((_VAL) & ~31) << 1)
 #elif DBPP == 32
-#define PMAKE(_VAL)  (((_VAL&(31<<10))<<9)|((_VAL&(31<<5))<<6)|((_VAL&31)<<3))
+# if SDL_BYTEORDER == SDL_LIL_ENDIAN && defined(MACOSX) /* Mac OS X Intel builds use a weird RGBA order (alpha in the low 8 bits) */
+#  define PMAKE(_VAL)  (((_VAL&(31u<<10u))<<1u)|((_VAL&(31u<<5u))<<14u)|((_VAL&31u)<<27u))
+# else
+#  define PMAKE(_VAL)  (((_VAL&(31u<<10u))<<9u)|((_VAL&(31u<<5u))<<6u)|((_VAL&31u)<<3u))
+# endif
 #endif
 #define SRCTYPE Bit16u
 #endif
@@ -107,11 +111,15 @@
 #if SBPP == 16
 #define SC scalerSourceCache.b16
 #if DBPP == 15
-#define PMAKE(_VAL) (((_VAL&~31)>>1)|(_VAL&31))
+#define PMAKE(_VAL) (((_VAL&~31u)>>1u)|(_VAL&31u))
 #elif DBPP == 16
 #define PMAKE(_VAL) (_VAL)
 #elif DBPP == 32
-#define PMAKE(_VAL)  (((_VAL&(31<<11))<<8)|((_VAL&(63<<5))<<5)|((_VAL&31)<<3))
+# if SDL_BYTEORDER == SDL_LIL_ENDIAN && defined(MACOSX) /* Mac OS X Intel builds use a weird RGBA order (alpha in the low 8 bits) */
+#  define PMAKE(_VAL)  (((_VAL&(31u<<11u))<<0u)|((_VAL&(63u<<5u))<<13u)|((_VAL&31u)<<27u))
+# else
+#  define PMAKE(_VAL)  (((_VAL&(31u<<11u))<<8u)|((_VAL&(63u<<5u))<<5u)|((_VAL&31u)<<3u))
+# endif
 #endif
 #define SRCTYPE Bit16u
 #endif
